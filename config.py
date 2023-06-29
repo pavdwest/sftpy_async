d3ae65a3e415ddda924fde88515c6f47258d0dc1
pavdwest <pavdwesthuizen@gmail.com>
2023-06-29 18:01:16 +0200

    Convert task to class and add retry logic.

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 import yaml
 
 
-with open('config.yaml', 'r') as file:
+config_file = 'config.staging.pwn.yaml'
+
+with open(config_file, 'r') as file:
     config = yaml.safe_load(file)
 
 batch_size = 10
+max_batch_retries = 5
 local_downloads_root_folder = config['local_downloads_root_folder']
 remote_folder_to_download = config['remote_folder_to_download']
 store_files_by_session = config['store_files_by_session']
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import asyncio
 import os
 from pathlib import Path
 from datetime import datetime
+from dataclasses import dataclass
+from typing import List
 
 from logger import get_logger
 import config
@@ -13,38 +15,59 @@ logger = get_logger(filename=timestamp)
 concurrency_limit = asyncio.Semaphore(config.batch_size)
 
 
-async def download_file(sftp, file: str, localdir: str, idx: int, total_count: int):
-    # TODO: Delete after download
-    
-    local_file_path = Path(f"{localdir}/{file}")
-    if not os.path.exists(local_file_path.parent):
-        logger.warning(f"Creating folder: {local_file_path.parent}")
-        os.makedirs(local_file_path.parent)
+@dataclass
+class DownloadFileTask:
+    sftp: asyncssh.SFTPClient
+    file: str
+    localdir: str
+    idx: int
+    total_count: int
+    completed: bool = False
 
-    async with concurrency_limit:
-        try:
-            logger.info(f"Starting download ({idx}/{total_count}): {file}")
-            await sftp.get(file, localpath=f"{localdir}{file}")
-            logger.info(f"Completed download ({idx}/{total_count}): {file}")
-        except Exception as ex:
-            logger.error("Connection likely closed unexpectedly. Restarting the process would likely resolve the issue.")
-            raise ex
+    async def execute(self):
+        local_file_path = Path(f"{self.localdir}/{self.file}")
+        if not os.path.exists(local_file_path.parent):
+            logger.warning(f"Creating folder: {local_file_path.parent}")
+            os.makedirs(local_file_path.parent)
+        async with concurrency_limit:
+            try:
+                logger.info(f"Starting download ({self.idx}/{self.total_count}): {self.file}")
+                await self.sftp.get(self.file, localpath=f"{self.localdir}{self.file}")
+                logger.info(f"Completed download ({self.idx}/{self.total_count}): {self.file}")
+                self.completed = True
+            except Exception as ex:
+                logger.error('Connection likely closed unexpectedly. Restarting the process would likely resolve the issue.')
 
 
 async def main():
     async with asyncssh.connect(
-        config.sftp_host, 
-        username=config.sftp_username, 
+        config.sftp_host,
+        username=config.sftp_username,
         password=config.sftp_password,
         known_hosts=None,
     ) as conn:
         logger.info(f"Session timestamp: {timestamp}")
         async with conn.start_sftp_client() as sftp:
             files = await sftp.glob(f"/{config.remote_folder_to_download}/*")
-            logger.info(f"Downloading {len(files)} files...")
+            logger.info(f"Downloading {len(files)} files with concurrency {concurrency_limit._value}...")
             local_dir = os.path.join(config.local_downloads_root_folder, timestamp) if config.store_files_by_session else config.local_downloads_root_folder
-            tasks = [download_file(sftp, file, localdir=local_dir, idx=i+1, total_count=len(files)) for i, file in enumerate(files)]
-            await asyncio.gather(*tasks)
+            tasks = [
+                DownloadFileTask(
+                    sftp=sftp,
+                    file=file,
+                    localdir=local_dir,
+                    idx=i+1,
+                    total_count=len(files),
+                ) for i, file in enumerate(files)
+            ]
+
+            retries = 0
+            while len([task for task in tasks if not task.completed]) > 0:
+                execs = [task.execute() for task in tasks if not task.completed]
+                res = await asyncio.gather(*execs)
+                if 0 <= retries <= config.max_batch_retries:
+                    print('Retrying failed tasks...')
+                    retries += 1
 
     logger.info('All done')
 
